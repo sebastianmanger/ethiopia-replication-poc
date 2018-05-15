ea982d6e6a15d001ed61aeb3f7dd995bb37e8499
Sebastian Manger <s.manger@gmx.ch>
2018-05-15 12:01:11 +0200

    Introduce row filtering for 'public' data

--- nodes/district_one/setup.sh
+++ nodes/district_one/setup.sh
@@ -7,19 +7,23 @@ psql -U postgres -d test -c "SELECT pglogical.create_node(
     dsn := 'host=district_one port=5432 dbname=test user=replication'
 );"
 
-# Create a dummy table and ond some data - which will be synchronized.
+# Create a dummy table.
+psql -U postgres -d test -c "DROP TABLE IF EXISTS slm"
 psql -U postgres -d test -c "CREATE TABLE slm (
     uuid    integer primary key,
+    public  bool,
     name    varchar(40)
 );"
 # This is the data that is 'replicated' to all nodes that have a subscription to this node.
-psql -U postgres -d test -c "INSERT INTO slm values (1, 'some name');"
+psql -U postgres -d test -c "INSERT INTO slm values (1, true, 'district one: first public entry');"
+psql -U postgres -d test -c "INSERT INTO slm values (2, false, 'district one: first non-public entry');"
 
 
 # Provide data for 'subscribers' (in this case: region_one)
-psql -U postgres -d test -c "select pglogical.create_replication_set('replication_set');"
+psql -U postgres -d test -c "select pglogical.create_replication_set('replicate_db_test');"
 psql -U postgres -d test -c "select pglogical.replication_set_add_table(
-  set_name := 'replication_set',
+  set_name := 'replicate_db_test',
   relation := 'slm',
-  synchronize_data := true
+  synchronize_data := true,
+  row_filter := 'public = true'
 );"
--- nodes/region_one/setup.sh
+++ nodes/region_one/setup.sh
@@ -7,17 +7,21 @@ psql -U postgres -d test -c "SELECT pglogical.create_node(
     dsn := 'host=region_one port=5432 dbname=test user=replication'
 );"
 
-# Create a dummy table
+# Create a dummy table.
+psql -U postgres -d test -c "DROP TABLE IF EXISTS slm"
 psql -U postgres -d test -c "CREATE TABLE slm (
     uuid    integer primary key,
+    public  bool,
     name    varchar(40)
 );"
 
 # Subscribe to the node 'district_one'
+# 'subscription_name' must match the name of the subscription (so this should be unique amongst all nodes)
+# 'replication_sets' must match the name of the set on district node
 psql -U postgres -d test -c "SELECT pglogical.drop_subscription('district_one_test', true);"
 psql -U postgres -d test -c "SELECT pglogical.create_subscription(
     subscription_name := 'district_one_test',
-    replication_sets := array['replication_set'],
+    replication_sets := array['replicate_db_test'],
     provider_dsn := 'host=district_one port=5432 dbname=test user=replication'
 );"
 
@@ -25,6 +29,6 @@ psql -U postgres -d test -c "SELECT pglogical.create_subscription(
 psql -U postgres -d test -c "SELECT pglogical.drop_subscription('district_two_test', true);"
 psql -U postgres -d test -c "SELECT pglogical.create_subscription(
     subscription_name := 'district_two_test',
-    replication_sets := array['replication_set'],
+    replication_sets := array['replicate_db_test'],
     provider_dsn := 'host=district_two port=5432 dbname=test user=replication'
 );"
